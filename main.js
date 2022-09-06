a03b070e0dcb8b288e448b50bda116c17476730d
zakiazhari <78516830+zakiazhari@users.noreply.github.com>
2022-09-06 15:13:27 +0700

    Generating black background

--- main.js
+++ main.js
@@ -5,12 +5,29 @@ function main() {
     var vertexShaderCode = 
     'void main ()  {' +
     "}";
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
 
     //fragment shader
     var fragmentShaderCode = `
         void main() {
-            
+
         }
     
     `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+
+    var ShaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT)
 }
