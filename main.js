5f035410ceb7c9becd92d7b990f1bf23bbf88fd2
zakiazhari <78516830+zakiazhari@users.noreply.github.com>
2022-09-06 14:46:31 +0700

    Adding veertex and shader codes

---
+++ main.js
@@ -0,0 +1,16 @@
+function main() {
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+    //vertex shader
+    var vertexShaderCode = 
+    'void main ()  {' +
+    "}";
+
+    //fragment shader
+    var fragmentShaderCode = `
+        void main() {
+            
+        }
+    
+    `;
+}
