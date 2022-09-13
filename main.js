91a2fb7d1d486bac57a15035495386a740551757
zakiazhari <78516830+zakiazhari@users.noreply.github.com>
2022-09-13 14:35:56 +0700

    week3

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
 function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
-    //vertex shader
-    var vertexShaderCode = 
-    'void main ()  {' +
-    "}";
+
+    // VERTEX SHADER
+    var vertexShaderCode = `
+        void main () {
+            gl_PointSize = 10.0;
+            gl_Position = vec4(0.5, 0.5, 1.0, 1.0);
+            // gl_Position is the final destination for storing
+            //  positional data for the rendered vertex
+        }
+    `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    //fragment shader
+    // FRAGMENT SHADER
     var fragmentShaderCode = `
         void main() {
-
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+            // gl_FlagColor is the final destination for storing
+            //  color data for the rendered fragment
         }
-    
     `;
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
+    // Comparing to C-Programming, we may imagine
+    //  that up to this step we have created two
+    //  object files (.o), for the vertex and fragment shaders
 
-    var ShaderProgram = gl.createProgram();
+    var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 0.75,   0.79,  1.0);
+                //Red, Green, Blue, Alpha
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.drawArrays(gl.POINT, 0, 1);
 }
