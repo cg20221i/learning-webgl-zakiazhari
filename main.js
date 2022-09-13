b85e614ac97b745e8e03edef1b4f30eac578b67c
zakiazhari <78516830+zakiazhari@users.noreply.github.com>
2022-09-13 14:46:47 +0700

    week3

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5,
+        0.0, 0.0,
+        -0.5, 0.5
+    ];
+
+    // Create a linked-list for storing the vertices data in the GPU realm
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+
     // VERTEX SHADER
     var vertexShaderCode = `
+        attribute vec2 aPosition;
         void main () {
             gl_PointSize = 10.0;
-            gl_Position = vec4(0.5, 0.5, 1.0, 1.0);
+            gl_Position = vec4(aPosition, 0.0, 1.0);
             // gl_Position is the final destination for storing
             //  positional data for the rendered vertex
         }
@@ -37,9 +49,13 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(1.0, 0.75,   0.79,  1.0);
                 //Red, Green, Blue, Alpha
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINT, 0, 1);
+    gl.drawArrays(gl.POINT, 0, 3);
 }
